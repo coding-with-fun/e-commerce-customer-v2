d5ab8a7886dcaf0f4bc15f40f6526d7b5ec6e2a4
Harrsh Patel <dev.harrsh@gmail.com>
2023-05-10 12:54:55 +0530

    Added logo

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,7 +1,11 @@
 import { Box } from '@mui/material';
 
 const Navbar = () => {
-    return <Box>Navbar</Box>;
+    return (
+        <Box className="h-20 absolute top-0 right-0 left-0 shadow-sm">
+            Navbar
+        </Box>
+    );
 };
 
 export default Navbar;
