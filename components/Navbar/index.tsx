e3c2e2804857623ae21395469acad671cc8983da
Harrsh Patel <dev.harrsh@gmail.com>
2023-05-10 14:56:23 +0530

    Added CSS

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,9 +1,14 @@
 import { Box } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
+import Logo from '../../public/assets/logo/logo-no-background.svg';
 
 const Navbar = () => {
     return (
-        <Box className="h-20 absolute top-0 right-0 left-0 shadow-sm">
-            Navbar
+        <Box className="h-16 absolute top-0 right-0 left-0 shadow-sm flex items-center justify-between px-8 z-50">
+            <Link href="/">
+                <Image height={25} src={Logo} alt="Avira" />
+            </Link>
         </Box>
     );
 };
